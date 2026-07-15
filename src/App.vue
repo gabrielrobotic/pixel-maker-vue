b1b5cc02ea59fc8f037692c77b5e3cb953212727
Batista dos Santos, Gabriel <gabrielrobotic@gmail.com>
2026-07-15 10:55:19 -0300

    chore: initial Vue project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="flex h-screen items-center justify-center">
+    <h1 class="text-3xl font-bold">Pixel Maker</h1>
+  </main>
+</template>
+
+<style scoped></style>
